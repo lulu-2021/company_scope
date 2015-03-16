dd9330397ecd455927181e49e1b1ee7ecc25aeda
Steve Forkin <steve.forkin@gmail.com>
2015-03-16 17:36:16 +1100

    put the rescue from company error methods into their own module that is included into the rails controller stack

--- lib/company_scope/control.rb
+++ lib/company_scope/control.rb
@@ -1,17 +1,5 @@
 module CompanyScope
   module Control
     class CompanyAccessViolationError < SecurityError; end
-
-    # - rescue from errors relating to the wrong company to avoid cross company data leakage
-    rescue_from CompanyScope::Control::CompanyAccessViolationError, with: :company_scope_company_not_set
-
-    def company_scope_company_not_set
-      flash[:error] = t('application.warnings.company_not_set')
-      if wrong_company_path.nil?
-        redirect_to(root_path) and return
-      else
-        redirect_to(wrong_company_path) and return
-      end
-    end
   end
 end
--- lib/company_scope/controller_filter.rb
+++ lib/company_scope/controller_filter.rb
@@ -4,6 +4,7 @@ module CompanyScope
   module ControllerFilter
     #
     def self.included(base)
+      base.extend(ControllerClassMethods)
       base.extend(ControllerFilterClassMethods)
     end
 
@@ -18,6 +19,20 @@ module CompanyScope
         yield
       ensure
         Company.current_id = nil
+      end
+    end
+
+    module ControllerClassMethods
+      # - rescue from errors relating to the wrong company to avoid cross company data leakage
+      rescue_from CompanyScope::Control::CompanyAccessViolationError, with: :company_scope_company_not_set
+
+      def company_scope_company_not_set
+        flash[:error] = t('application.warnings.company_not_set')
+        if wrong_company_path.nil?
+          redirect_to(root_path) and return
+        else
+          redirect_to(wrong_company_path) and return
+        end
       end
     end
   end
