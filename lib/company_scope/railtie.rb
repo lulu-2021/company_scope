1ca2d78095a9c14bee1f191e930aa11798ae0f54
Steve Forkin <steve.forkin@gmail.com>
2015-03-16 17:46:27 +1100

    fixed the wrong naming of the rails railtie class

--- lib/company_scope/railtie.rb
+++ lib/company_scope/railtie.rb
@@ -2,21 +2,21 @@ require 'company_scope'
 require 'rails'
 #
 module CompanyScope
-  class RailTie < Rails::Railtie
+  class Railtie < Rails::Railtie
     #
     initializer :after_initialize do
       # - the base module injects the default scope into company dependant models
       ActiveRecord::Base.send(:include, CompanyScope::Base)
 
       # - the company_entity module injects class methods for acting as the company!
-      ActiveRecord::Base.send(:include, CompanyScope::CompanyEntity)
+      ActiveRecord::Base.send(:include, CompanyScope::Guardian)
 
       # - the control module has some error handling for the application controller
       ActionController::Base.send(:include, CompanyScope::Control)
 
       # - the controller_filter module injects an around filter to ensure all
       # - actions in the controller are wrapped
-      ActionController::Base.send(:include, CompanyScope::ControllerFilter)
+      ActionController::Base.send(:include, CompanyScope::Filter)
     end
     #
   end
