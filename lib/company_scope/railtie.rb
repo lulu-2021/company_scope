3dfaa83d7d2b32ff08f349a3936ee4457dab612b
Steve Forkin <steve.forkin@gmail.com>
2015-03-16 15:56:32 +1100

    fixed the proper rails rail-tie dependancy

--- lib/company_scope/railtie.rb
+++ lib/company_scope/railtie.rb
@@ -2,9 +2,11 @@ require 'company_scope'
 require 'rails'
 #
 module CompanyScope
-  class RailTie
+  class RailTie < Rails::RailTie
+    #
     initializer :after_initialize do
       ActiveRecord::Base.send(:include, CompanyScope::Base)
     end
+    #
   end
 end
