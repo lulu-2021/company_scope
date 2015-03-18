d40ec871374f63452f02bb7cc7885cb6dd64ca54
Steve Forkin <steve.forkin@gmail.com>
2015-03-18 20:26:21 +1100

    added a few tests to check that the acts_as_guardian method and current_id is added to company correctly

--- spec/company_scope/company_scope_guardian_spec.rb
+++ spec/company_scope/company_scope_guardian_spec.rb
@@ -3,9 +3,18 @@ require 'active_record_models'
 #
 describe CompanyScope::Guardian do
 
-  Given(:company) { Company.new }
+  Given!(:default_company_name) { 'DEFAULT' }
+
+  Given!(:setup) {
+    company = Company.create(company_name: default_company_name )
+    Company.current_id = company.id
+    company.id
+  }
 
   context 'checking whether the correct methods have been injected' do
+    Given(:retrieve_company) { Company.where(company_name: default_company_name).first }
 
+    Then { expect(retrieve_company.class).to respond_to(:acts_as_guardian) }
+    Then { expect(retrieve_company.class.current_id).to eq Company.current_id }
   end
 end
