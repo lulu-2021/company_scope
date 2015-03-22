c0052373a631e70952f11a13c9787a57c4897150
Steve Forkin <steve.forkin@gmail.com>
2015-03-22 22:31:04 +1100

    fixed some issues with the way bad company exceptions are caught

--- lib/company_scope/filter.rb
+++ lib/company_scope/filter.rb
@@ -54,11 +54,6 @@ module CompanyScope
       def company_scope_company_not_set
         flash[:error] = t('application.warnings.company_not_set')
         return head(:forbidden)
-        #if wrong_company_path.nil?
-        #  redirect_to(root_path) and return
-        #else
-        #  redirect_to(wrong_company_path) and return
-        #end
       end
     end
   end
--- lib/rack/multi_company.rb
+++ lib/rack/multi_company.rb
@@ -8,7 +8,8 @@ module Rack
     def initialize(app, company_class)
       @app = app
       @company_class_name = company_class.to_s.split('_').collect!{ |w| w.capitalize }.join
-      @company = Module.const_get(@company_class_name).find_by_company_name('DEFAULT') if db_configured
+      company = Module.const_get(@company_class_name).find_by_company_name('DEFAULT') if db_configured
+      @company = company.first unless company.nil?
     end
 
     def call(env)
@@ -17,7 +18,8 @@ module Rack
       # disallow any non alphabetic chars!
       domain = first_sub_domain.upcase if first_sub_domain.match(/\A[a-zA-Z0-9]*\z/)
       # insert the company into ENV - for the controller helper_method 'current_company'
-      env['COMPANY_ID'] = retrieve_company_from_subdomain(domain).id
+      company = retrieve_company_from_subdomain(domain)
+      env['COMPANY_ID'] = company.id unless company.nil?
       puts "\n\n Rack MultiCompany setting COMPANY_ID env: #{env['COMPANY_ID']}"
       response = @app.call(env)
       response
@@ -31,11 +33,11 @@ module Rack
 
     def retrieve_company_from_subdomain(domain)
       # - During test runs we load a company called 'DEFAULT' - it does not need to exist during initialisation
-      @company = Module.const_get(@company_class_name).find_by_company_name('DEFAULT') if Rails.env == 'test'
+      company = Module.const_get(@company_class_name).find_by_company_name('DEFAULT') if Rails.env == 'test'
       # - only ever load the company when the subdomain changes - also works when company is nil from an unsuccessful attempt..
-      @company = Module.const_get(@company_class_name).find_by_company_name(domain).first unless ( domain == @company.company_name )
-      raise CompanyScope::Control::CompanyAccessViolationError if @company == nil
-      @company
+      company = Module.const_get(@company_class_name).find_by_company_name(domain) unless ( domain == @company.company_name )
+      raise CompanyScope::Control::CompanyAccessViolationError if company == nil
+      @company = company.first unless company.nil?
     end
   end
 end
