fc212afbbcac3c502147a95c870e4a25ff54e9ef
Steve Forkin <steve.forkin@gmail.com>
2015-03-23 10:49:22 +1100

    fixed an issue with Rack::File

--- lib/rack/company_error.rb
+++ lib/rack/company_error.rb
@@ -14,8 +14,8 @@ module Rack
       rescue CompanyScope::Control::CompanyAccessViolationError => error
         error_output = "You tried to access a company that does not exist : #{error}"
         error_file = "#{error_path}/invalid_company.html"
-        if File.file?(error_file) && File.readable?(error_file)
-          return render_format(status, 'text/html', File.read(path))
+        if ::File.exist?(error_file)
+          return render_format(status, 'text/html', ::File.read(path))
         else
           return [404, { "X-Cascade" => "pass" }, [ { status: 404, error: error_output }.to_html ]]
         end
