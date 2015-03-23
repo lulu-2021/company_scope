96d8183ea717257a3daf66211ec97e2d440992da
Steve Forkin <steve.forkin@gmail.com>
2015-03-23 12:18:45 +1100

    dumb error - garrgh

--- lib/custom/company_filter.rb
+++ lib/custom/company_filter.rb
@@ -12,7 +12,7 @@ module Custom
       invalid_company_middleware = Custom::CompanyError.new(@app, @company_class_name)
       # - always call the MultiCompany Middleware!
       env = multi_company_middleware.call(env)
-      if env['COMPANY_SCOPE_ERROR'] = 'INVALID_COMPANY_ERROR'
+      if env['COMPANY_SCOPE_ERROR'] == 'INVALID_COMPANY_ERROR'
         # - only call the InvalidCompany middleware when we have an issue!
         env = invalid_company_middleware.call(env)
       end
