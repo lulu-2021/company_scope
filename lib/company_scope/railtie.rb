32a52e0e63ad0e7360ec78cabb8257a74b1a60ec
Steve Forkin <steve.forkin@gmail.com>
2015-03-24 11:30:30 +1100

    changed the place the rack multi-company middleware is loaded to work with rails and rails-api.

--- lib/company_scope/railtie.rb
+++ lib/company_scope/railtie.rb
@@ -11,16 +11,16 @@ module CompanyScope
         config.company_name_matcher = app.config.company_scope[:company_name_matcher] || :subdomain_matcher
       end
       #
-      # - add MultiCompany Rack middleware to detect the company_name from the subdomain
-      #app.config.middleware.insert_after Rack::Sendfile, Custom::MultiCompany, company_config, company_name_matcher
-      app.config.middleware.insert_before ActionDispatch::Static, Custom::MultiCompany, company_config, company_name_matcher
-      #
       company_config = CompanyScope.config.company_model
       company_name_matcher = CompanyScope.config.company_name_matcher
       #
+      # - add MultiCompany Rack middleware to detect the company_name from the subdomain
+      #app.config.middleware.insert_after Rack::Sendfile, Custom::MultiCompany, company_config, company_name_matcher
+      app.config.middleware.insert_before ActionDispatch::Static, Custom::MultiCompany, company_config, company_name_matcher
       # - the base module injects the default scope into company dependant models
+      #
       ActiveRecord::Base.send(:include, CompanyScope::Base)
-
+      #
       # - the company_entity module injects class methods for acting as the company!
       ActiveRecord::Base.send(:include, CompanyScope::Guardian)
       #
