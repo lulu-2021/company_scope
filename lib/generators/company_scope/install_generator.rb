77684cf4986c049f0cd964dce8f7cef6104150d8
Steve Forkin <steve.forkin@gmail.com>
2015-07-03 10:11:45 +1000

    added an install generator to add the application wide config setting to enable and disable company_scope gem settings

--- lib/generators/company_scope/install_generator.rb
+++ lib/generators/company_scope/install_generator.rb
@@ -3,7 +3,7 @@ module CompanyScope
   class InstallGenerator < Rails::Generators::Base
     desc "CompanyScope installs the relevant database migrations"
     source_root File.expand_path("../templates", __FILE__)
-    
+
     class_option :company_class_name, :type => :string, :default => 'company', :desc => "Name of Company Model"
     class_option :user_class_name, :type => :string, :default => 'user', :desc => "Name of User Model"
     class_option :enable_uuid, :type => boolean, :default => false, :desc => "Enable UUID if the extension is used"
@@ -13,6 +13,11 @@ module CompanyScope
     end
 
     def modify_application_rb
+      line = "class Application < Rails::Application"
+      gsub_file 'config/environments/production.rb', /(#{Regexp.escape(line)})/mi do |match|
+        "class Application < Rails::Application"
+        "  config.company_scope[:configured] = false"
+      end
     end
 
     def create_migrations
