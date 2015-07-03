09024f06ac8d54ba1abcdc972c73add1942b4504
Steve Forkin <steve.forkin@gmail.com>
2015-07-03 16:20:17 +1000

    added an install generator to add the application wide config setting to enable and disable company_scope gem settings

--- lib/generators/company_scope/install_generator.rb
+++ lib/generators/company_scope/install_generator.rb
@@ -30,7 +30,7 @@ module CompanyScope
     def generate_company_migration
       unless options.no_migrations?
         # - generate a company model and migration with a 50 char max on the company name and unique index
-        generate(:model, ':company id: :uuid', 'company_name:string{50}:uniq' )
+        generate(:model, 'company id: :uuid', 'company_name:string{50}:uniq' )
       end
     end
 
