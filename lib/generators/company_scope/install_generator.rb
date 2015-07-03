05c0630a77ecfe6d8d40fa0ad84467d13b049947
Steve Forkin <steve.forkin@gmail.com>
2015-07-03 12:13:06 +1000

    added an install generator to add the application wide config setting to enable and disable company_scope gem settings

--- lib/generators/company_scope/install_generator.rb
+++ lib/generators/company_scope/install_generator.rb
@@ -7,6 +7,7 @@ module CompanyScope
     class_option :company_class_name, :type => :string, :default => 'company', :desc => "Name of Company Model"
     class_option :user_class_name, :type => :string, :default => 'user', :desc => "Name of User Model"
     class_option :enable_uuid, :type => :boolean, :default => false, :desc => "Enable UUID if the extension is used"
+    class_option :no_migrations, :type => :boolean, :default => false, :desc => "Disable the creation of migrations"
 
     def self.source_root
       File.expand_path("../templates", __FILE__)
@@ -15,16 +16,22 @@ module CompanyScope
     def modify_application_rb
       # add the company_scope configuration enabler into config/application.rb
       line = "class Application < Rails::Application"
+      check_config = "config.company_scope[:configured]"
+      insert_line = "#{check_config} = false"
+      #unless
       gsub_file 'config/application.rb', /(#{Regexp.escape(line)})/mi do |match|
-        "#{match}\n    config.company_scope[:configured] = false"
+        # "#{match}\n    #{insert_line}"
+        match << "#{insert_line}"
       end
     end
 
     def create_migrations
-      Dir["#{self.class.source_root}/migrations/*.erb"].sort.each do |filepath|
-        name = File.basename(filepath)
-        template "migrations/#{name}", "db/migrate/#{name}"
-        sleep 1
+      unless options.no_migrations?
+        Dir["#{self.class.source_root}/migrations/*.erb"].sort.each do |filepath|
+          name = File.basename(filepath)
+          template "migrations/#{name}", "db/migrate/#{name}"
+          sleep 1
+        end
       end
     end
   end
